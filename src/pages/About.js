93859cd295399f674a9b5084869dfc9b6722f4d0
Akash-wq-css <akawinmail@gmail.com>
2021-03-25 18:19:24 +0530

    intial commit

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores tempora repudiandae praesentium minus natus hic, aut illo fugiat cum eum? Magnam accusantium est, provident esse fugiat laudantium vel. Sed illum sequi ad minima. Quis officiis deserunt magni consequatur rerum minima fuga assumenda atque esse, voluptate eligendi aliquid sapiente sed dolor?
+      </p>
+
+    </section>
+  )
+}
+
+export default About
